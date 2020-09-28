9e90c0b7fb728ec617c4716b72aab4fb555718fc
Greg155 <gregakhun@gmail.com>
2020-09-28 16:11:06 +0200

    Workshope function fat arrow

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const sayHello = () => console.log("Hello World");
+sayHello();
+
+let returnParameter = (value) => value; 
+console.log(returnParameter(5));
+
+let doubleValue = (value) => value *2;
+console.log(doubleValue(5));
+
+let mutlication = (a,b) => a * b;
+console.log(mutlication(2,9));
+
+let stringWithStar = (string) => console.log("* "+string+" *");
+stringWithStar("Greg");
