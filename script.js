c1b12929d081fcd16888bd9208d6f2b6296e4045
Greg155 <gregakhun@gmail.com>
2020-09-28 16:57:48 +0200

    Workshop function update

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 
-const sayHello = () => console.log("Hello World");
-sayHello();
+const sayHelloWorld = () => console.log("Hello World");
+sayHelloWorld();
 
-let returnParameter = (value) => value; 
+let returnParameter = (value = "No parameter") => value; 
 console.log(returnParameter(5));
+console.log(returnParameter());
 
-let doubleValue = (value) => value *2;
-console.log(doubleValue(5));
+let double = (value) => value * 2;
+console.log(double(5));
 
 let mutlication = (a,b) => a * b;
 console.log(mutlication(2,9));
 
 let stringWithStar = (string) => console.log("* "+string+" *");
 stringWithStar("Greg");
+
+let returnType = (parameter) => {
+    let type = typeof(parameter)
+    if(type === "object"){
+        return "Array";
+    }else{
+        return type;
+    }   
+    
+}
+// Test function returnType
+
+let str=[];
+let a = 5;
+console.log(returnType(str));
+console.log(returnType(a));
